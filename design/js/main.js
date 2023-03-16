c4af8ab6519aaba8d61d9f2d70c7b0bf7c47e2fc
RavenDev <rostandsenguep@gmail.com>
2023-03-16 12:00:52 +0100

    un bon depart

---
+++ design/js/main.js
@@ -0,0 +1,36 @@
+var ancre = document.querySelector(".ancre");
+var lien = document.querySelector(".lien");
+
+var liens = document.querySelector(".liens");
+var produits = document.querySelector(".produits");
+var nav = document.querySelector(".navbar");
+var posi = nav.offsetTop;
+var sect = document.querySelectorAll(".sect1");
+var posi2 = sect.offsetTop;
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= posi) {
+        nav.classList.add('fixed');
+    }else{
+        nav.classList.remove('fixed');
+    }
+})
+
+produits.addEventListener("mouseover", () => {
+    ancre.classList.add('see');
+})
+liens.addEventListener("mouseover", () => {
+    lien.classList.add('see');
+})
+ancre.addEventListener("mouseover", () => {
+    ancre.classList.add('see');
+})
+lien.addEventListener("mouseover", () => {
+    lien.classList.add('see');
+})
+
+
+nav.addEventListener("mouseout", () => {
+    ancre.classList.remove('see');
+    lien.classList.remove('see');
+})
